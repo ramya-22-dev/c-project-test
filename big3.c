418ee75d5a598934760f92d596a277c46911e968
Ramya <ramya.e999@gmail.com>
2021-04-02 19:08:16 +0000

    edited big3.c

--- big3.c
+++ big3.c
@@ -6,15 +6,15 @@ void big3() {
 
     // if n1 is greater than both n2 and n3, n1 is the largest
     if (n1 >= n2 && n1 >= n3)
-        printf("%.2f is the largest number.", n1);
+        printf("%.2f is the Largest number.", n1);
 
     // if n2 is greater than both n1 and n3, n2 is the largest
     if (n2 >= n1 && n2 >= n3)
-        printf("%.2f is the largest number.", n2);
+        printf("%.2f is the Largest number.", n2);
 
     // if n3 is greater than both n1 and n2, n3 is the largest
     if (n3 >= n1 && n3 >= n2)
-        printf("%.2f is the largest number.", n3);
+        printf("%.2f is the Largest number.", n3);
 
     //return 0;
 }
